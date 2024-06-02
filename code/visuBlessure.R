9fd581f37c68fa16f42511c9f8cc1e65596a1a6c
knasme <ahmed06032003@gmail.com>
2024-06-02 17:02:42 +0200

    ajout un graph blessure

--- code/visuBlessure.R
+++ code/visuBlessure.R
@@ -45,3 +45,27 @@ palette <- colorRampPalette(c("blue", "red"))(n_colors)
 # Créer le nuage de mots
 wordcloud(names(freq), freq, min.freq = 1, max.words = 200, random.order = FALSE, colors = palette)
 title("Blessures joueurs Premier League")
+
+
+
+# Trier les fréquences par ordre décroissant
+freq_sorted <- sort(freq, decreasing = TRUE)
+
+# Sélectionner les 5 blessures les plus fréquentes
+top_5_injuries <- head(names(freq_sorted), 5)
+top_5_freq <- head(freq_sorted, 5)
+
+# Calculer les fréquences en pourcentages
+total_freq <- sum(freq)
+top_5_percent <- (top_5_freq / total_freq) * 100
+
+# Créer une palette de couleurs allant du gris au rouge
+colors <- colorRampPalette(c("gray", "red"))(25)
+
+# Créer le graphique à barres horizontal avec les couleurs en fonction des pourcentages
+barplot(top_5_percent, names.arg = top_5_injuries, main = "Top 5 des blessures en Premier League (en %)", xlab = "Fréquence (en %)", ylab = "Blessures", col = colors[top_5_percent], horiz = TRUE, xlim = c(0, 12), cex.names = 1, cex.axis = 1)
+top_5_percent<-round(top_5_percent,1)
+
+# Ajouter les valeurs précises des pourcentages à l'intérieur des barres
+text(x = top_5_percent - 0.3, y = 1:5, labels = top_5_percent, cex = 0.8, pos = 3, srt = 0, font = 2)
+
